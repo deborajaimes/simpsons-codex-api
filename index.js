7b78da8662e6dc265e36b4223c0543968c0f9565
Debora Jaimes <deboranjaimes@gmail.com>
2021-12-11 15:38:37 -0400

    node

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ api.use(cors({ // Habilita o CORS (Cross Origin Resource Sharing)
     origin: "*"
 }))
 
+const PORT = process.env.PORT || 3001
+
+
 const personagens = [
     {
         nome: "Homer Simpsons",
@@ -31,6 +34,6 @@ api.post("/personagens", (req, res) => {
     res.json({ mensagem: "Personagem salvo com sucesso!" })
 })
 
-api.listen(3001, () => {
-    console.log("API rodando na porta 3000")
+api.listen(PORT, () => {
+    console.log("API rodando na porta" + PORT)
 })
